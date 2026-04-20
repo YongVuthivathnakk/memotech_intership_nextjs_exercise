5b13dfe3271df618f4cedf489be5b1ae11e0bc57
Nakk <vuthivathnakkyong@gmail.com>
2026-04-20 11:52:19 +0700

    Exercise 2 - add about compoent to /blog

--- my-exercise-app/src/app/blog/page.tsx
+++ my-exercise-app/src/app/blog/page.tsx
@@ -1,18 +1,22 @@
 import React from "react";
+import AboutPage from "../about/page";
 
 export default function Blog() {
   return (
     <div>
       <div>
         <a href="#">Title 1</a>
-        <img src="https://placehold.co/600x400" alt="" />
+        <img src="https://placehold.co/400" alt="" />
       </div>
       <br />
       <br />
+
       <div>
         <a href="#">Title 2</a>
-        <img src="https://placehold.co/600x400" alt="" />
-      </div>
+        <img src="https://placehold.co/400" alt="" />
+          </div>
+          
+          <AboutPage/>
     </div>
   );
 }
