72b8a4e8efffd1820c740158d3a676d012bf2dc0
Nakk <vuthivathnakkyong@gmail.com>
2026-04-20 11:35:46 +0700

    Exercise 2 - add /about and /bog to the /app folder

---
+++ my-exercise-app/src/app/blog/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Blog() {
+  return (
+    <div>
+      <div>
+        <a href="#">Title 1</a>
+        <img src="https://placehold.co/600x400" alt="" />
+      </div>
+      <br />
+      <br />
+      <div>
+        <a href="#">Title 2</a>
+        <img src="https://placehold.co/600x400" alt="" />
+      </div>
+    </div>
+  );
+}
